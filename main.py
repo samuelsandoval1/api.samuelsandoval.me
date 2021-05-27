a2e8940f4405754d2bf91ad897ceb59bd684fab5
Samuel Sandoval <sjsandoval1000@gmail.com>
2021-05-27 11:16:50 -0700

    updated app

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import os
 
 app = create_app()
 def main():
-    PORT = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=PORT)
+    # PORT = int(os.environ.get('PORT', 5000))
+    # app.run(host='0.0.0.0', port=PORT)
+    app.run()
 
 if __name__ == '__main__':
     main()
